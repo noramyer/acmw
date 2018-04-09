66cf5b6b3eafd34bd0376eee74fc1d6fc5802c0c
noramyer <myer.41@osu.edu>
2018-04-09 19:22:29 -0400

    Script for bubble chart

---
+++ js/d3/bubbleChart.js
@@ -0,0 +1,77 @@
+var diameter = 960,
+    format = d3.format(",d"),
+    color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+    var jsonData = {
+     name: "flare",
+     children: [
+      {
+       name: "scale",
+       children: [
+        { name: "IScaleMap", size: 20},
+        { name: "LinearScale", size: 15},
+        { name: "LogScale", size: 13},
+        { name: "OrdinalScale", size: 15},
+        { name: "QuantileScale", size: 17},
+        { name: "QuantitativeScale", size: 35},
+        {name: "RootScale", size: 23},
+        {name: "Scale", size: 5},
+        {name: "ScaleType", size: 60},
+        {name: "TimeScale", size: 20}
+       ]
+     },
+     ]
+   };
+
+var bubble = d3.pack()
+    .size([1050, 700])
+    .padding(1.5);
+
+var svg = d3.select("#bubble-chart-container").append("svg")
+    .attr("width", 1050)
+    .attr("height", 700)
+    .attr("class", "bubble");
+
+parseData(jsonData);
+
+function parseData(data) {
+  var root = d3.hierarchy(classes(data))
+      .sum(function(d) { return d.value; })
+      .sort(function(a, b) { return b.value - a.value; });
+
+  bubble(root);
+  var node = svg.selectAll(".node")
+      .data(root.children)
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("title")
+      .text(function(d) { return d.data.className + ": " + format(d.value); });
+
+  node.append("circle")
+      .attr("r", function(d) { return d.r; })
+      .style("fill", function(d) {
+        return color(d.data.packageName);
+      });
+
+  node.append("text")
+      .attr("dy", ".3em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.className.substring(0, d.r / 3); });
+}
+
+// Returns a flattened hierarchy containing all leaf nodes under the root.
+function classes(root) {
+  var classes = [];
+
+  function recurse(name, node) {
+    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+    else classes.push({packageName: name, className: node.name, value: node.size});
+  }
+
+  recurse(null, root);
+  return {children: classes};
+}
+
+d3.select(self.frameElement).style("height", diameter + "px");
